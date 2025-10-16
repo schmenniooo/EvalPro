2f8983cdd7dd926f37408027cb70edcfdb0f2bfe
ennio <ennio.nick@gmail.com>
2025-10-16 10:05:16 +0200

    new cross platform gui project

---
+++ EvalProUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EvalProUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EvalProUI.Pages")]
