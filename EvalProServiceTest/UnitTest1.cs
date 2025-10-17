158a9d566028264a83d049f7d203e8b692736eb5
ennio <ennio.nick@gmail.com>
2025-10-17 12:21:42 +0200

    test project added

---
+++ EvalProServiceTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace EvalProServiceTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
