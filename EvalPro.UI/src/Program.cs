a387639530e42676ea9fa9150a7298e042ecb0dc
ennio <ennio.nick@gmail.com>
2026-01-07 08:26:44 +0100

    changed project type to use wpf instead of winforms

--- EvalPro.UI/src/Program.cs
+++ EvalPro.UI/src/Program.cs
@@ -8,9 +8,6 @@ static class Program
     [STAThread]
     static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new MainView());
+        
     }
 }
