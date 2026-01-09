5f96c502d8182d097baab56337578440dce66e15
ennio <ennio.nick@gmail.com>
2026-01-09 11:53:59 +0100

    audit committee attributes

--- EvalPro.Service/src/impl/model/AuditCommittee.cs
+++ EvalPro.Service/src/impl/model/AuditCommittee.cs
@@ -2,5 +2,16 @@ namespace EvalProService.impl.model;
 
 public class AuditCommittee
 {
+    public string Designation { get; set; }
     
+    public string ApprenticeShip { get; set; }
+    
+    public List<DateTime> TestDates { get; set; }
+    
+    public AuditCommittee(string designation, string apprenticeShip, List<DateTime> testDates)
+    {
+        Designation = designation;
+        ApprenticeShip = apprenticeShip;
+        TestDates = testDates;
+    }
 }
