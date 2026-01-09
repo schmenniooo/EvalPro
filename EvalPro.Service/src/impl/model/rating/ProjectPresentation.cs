11b8a267850cc087bfce3f7936eeef84ce49f2ca
ennio <ennio.nick@gmail.com>
2026-01-09 11:50:24 +0100

    using base rating for child classes

--- EvalPro.Service/src/impl/model/rating/ProjectPresentation.cs
+++ EvalPro.Service/src/impl/model/rating/ProjectPresentation.cs
@@ -1,6 +1,11 @@
+using EvalProService.impl.model.rating;
+
 namespace EvalProService.impl.model;
 
-public class ProjectPresentation
+public class ProjectPresentation : BaseRating
 {
-    
+    public ProjectPresentation(string finalComment, Dictionary<string, int> pointsPerCriteria, Dictionary<string, string> commentsPerCriteria)
+        : base(finalComment, pointsPerCriteria, commentsPerCriteria)
+    {
+    }
 }
--- EvalPro.Service/src/impl/model/rating/TechConversation.cs
+++ EvalPro.Service/src/impl/model/rating/TechConversation.cs
@@ -1,6 +1,11 @@
+using EvalProService.impl.model.rating;
+
 namespace EvalProService.impl.model;
 
-public class TechConversation
+public class TechConversation : BaseRating
 {
-    
+    public TechConversation(string finalComment, Dictionary<string, int> pointsPerCriteria, Dictionary<string, string> commentsPerCriteria)
+        : base(finalComment, pointsPerCriteria, commentsPerCriteria)
+    {
+    }
 }
