5d3829842a34b94961abfdeb81efd8a90dae963f
ennio <ennio.nick@gmail.com>
2026-01-09 11:55:36 +0100

    Examinee attributes added

--- EvalPro.Service/src/impl/model/Examinee.cs
+++ EvalPro.Service/src/impl/model/Examinee.cs
@@ -2,5 +2,19 @@ namespace EvalProService.impl.model;
 
 public class Examinee
 {
+    public string Name { get; set; }
     
+    public string Company { get; set; }
+    
+    public string ContactPerson { get; set; }
+    
+    public string ProjectTitle { get; set; }
+
+    public Examinee(string name, string company, string contactPerson, string projectTitle)
+    {
+        Name = name;
+        Company = company;
+        ContactPerson = contactPerson;
+        ProjectTitle = projectTitle;
+    }
 }
