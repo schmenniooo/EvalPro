e2ee0cd2061ec20cd6acb167fdda172c623814c6
ennio <ennio.nick@gmail.com>
2026-01-09 11:59:22 +0100

    lists of objects added

--- EvalPro.Service/src/impl/service/EvalProService.cs
+++ EvalPro.Service/src/impl/service/EvalProService.cs
@@ -1,12 +1,20 @@
 using EvalProService.api;
 using EvalProService.db;
+using EvalProService.impl.model;
 
 namespace EvalProService.impl;
 
 public class EvalProService : IServiceApi
 {
     private readonly ServiceDataController _dataController = new();
-
+    
+    private List<Examinee> _examineeslist = [];
+    private List<AuditCommittee> _committeeslist = [];
+    private List<ProjectDocumentation>  _projectDocumentationList = [];
+    private List<ProjectPresentation>  _projectPresentationList = [];
+    private List<TechConversation>  _projectTechConversationList = [];
+    private List<SupplementaryExamination>  _supplementaryExaminationList = [];
+    
     public void SaveConfig()
     {
         throw new NotImplementedException();
