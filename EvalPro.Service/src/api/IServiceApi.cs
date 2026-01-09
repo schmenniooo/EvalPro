3db19ef8c52a97e8e81f9d158078984945f3633e
ennio <ennio.nick@gmail.com>
2026-01-09 12:04:56 +0100

    pre declaring getters for lists in interface

--- EvalPro.Service/src/api/IServiceApi.cs
+++ EvalPro.Service/src/api/IServiceApi.cs
@@ -1,6 +1,17 @@
-﻿namespace EvalProService.api;
+﻿using EvalProService.impl.model;
+
+namespace EvalProService.api;
 
 public interface IServiceApi
 {
-    
+    List<AuditCommittee> GetCommitteesList();
+
+    List<ProjectDocumentation> GetProjectDocumentationList();
+
+    List<ProjectPresentation> GetProjectPresentationList();
+
+    List<TechConversation> GetProjectTechConversationList();
+
+    List<SupplementaryExamination> GetSupplementaryExaminationList();
+
 }
