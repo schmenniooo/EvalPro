49b246fd9de0e3c6c1beedfb57019fc162fd956e
ennio <ennio.nick@gmail.com>
2026-01-13 12:20:52 +0100

    rating references in examinee

--- EvalPro.Service/src/impl/model/Examinee.cs
+++ EvalPro.Service/src/impl/model/Examinee.cs
@@ -9,12 +9,25 @@ public class Examinee
     public string ContactPerson { get; set; }
     
     public string ProjectTitle { get; set; }
+    
+    public ProjectDocumentation ProjectDocumentation { get; set; }
+    
+    public ProjectPresentation ProjectPresentation { get; set; }
+    
+    public TechConversation TechConversation { get; set; }
+    
+    public SupplementaryExamination SupplementaryExamination { get; set; }
 
-    public Examinee(string name, string company, string contactPerson, string projectTitle)
+    public Examinee(string name, string company, string contactPerson, string projectTitle, ProjectDocumentation projectDocumentation, ProjectPresentation projectPresentation, TechConversation techConversation,  SupplementaryExamination supplementaryExamination)
     {
         Name = name;
         Company = company;
         ContactPerson = contactPerson;
         ProjectTitle = projectTitle;
+        
+        ProjectDocumentation = projectDocumentation;
+        ProjectPresentation = projectPresentation;
+        TechConversation = techConversation;
+        SupplementaryExamination = supplementaryExamination;
     }
 }
