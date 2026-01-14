aaf979cab733a62667a71b63a1989c5b35b4e5e7
ennio <ennio.nick@gmail.com>
2026-01-14 14:38:17 +0100

    SupplementaryExamination.cs class implemented

--- EvalPro.Service/src/impl/model/ratings/SupplementaryExamination.cs
+++ EvalPro.Service/src/impl/model/ratings/SupplementaryExamination.cs
@@ -2,5 +2,16 @@ namespace EvalProService.impl.model.ratings;
 
 public class SupplementaryExamination
 {
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
     
+    public string ChosenTestArea { get; set; }
+    public int points { get; set; }
+
+    public SupplementaryExamination(string chosenTestArea, int points)
+    {
+        ChosenTestArea = chosenTestArea;
+        points = points;
+    }
 }
