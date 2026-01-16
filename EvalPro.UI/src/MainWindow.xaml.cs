ad8d68feb3bdde306371a74af7f29e28ecea0e58
Dominik M. <dominik.macha@bmw.de>
2026-01-16 12:04:21 +0100

    Neues UI-Design

---
+++ EvalPro.UI/src/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace WPFApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        // WICHTIG: public PROPERTY, kein Feld!
+        public string GreetingName
+        {
+            get { return $"Guten Morgen, {GreetingName}"; }
+        }
+    }
+}
