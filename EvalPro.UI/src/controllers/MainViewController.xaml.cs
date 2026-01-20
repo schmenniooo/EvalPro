31409b8cdbe2888331eecbaeea0806e38a271e63
DominikBS3 <dominik.macha@bmw.de>
2026-01-20 12:41:32 +0100

    Feature UI test (#26)
    * Neues UI-Design
    
    * Save commit
    
    * File deleted

---
+++ EvalPro.UI/src/controllers/MainViewController.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace EvalProUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        // WICHTIG: public PROPERTY, kein Feld!
+        // Gefixt: Rückgabe war rekursiv und hat zu StackOverflow geführt.
+        public string GreetingName
+        {
+            get { return $"Guten Morgen, Benutzer"; }
+        }
+    }
+}
---
+++ EvalPro.UI/src/views/MainView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace EvalProUI;
+
+public partial class MainView : Window
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
