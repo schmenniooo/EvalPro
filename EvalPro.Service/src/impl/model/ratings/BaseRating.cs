e83c9119703542e8e6a25a8d8e7c8d8630e9ffc7
ennio <ennio.nick@gmail.com>
2026-03-23 16:20:14 +0100

    extending from base entity

--- EvalPro.Service/src/impl/model/ratings/BaseRating.cs
+++ EvalPro.Service/src/impl/model/ratings/BaseRating.cs
@@ -1,25 +1,19 @@
+using EvalProService.impl.model.entities;
+
 namespace EvalProService.impl.model.ratings;
 
-public class BaseRating
+public class BaseRating : BaseEntity
 {
     public string FinalComment { get; set; }
     
     public Dictionary<string, int> PointsPerCriteria { get; set; }
     
     public Dictionary<string, string> CommentsPerCriteria { get; set; }
-    
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    
-    public DateTime CreatedAt { get; set; }
-    
-    public DateTime UpdatedAt { get; set; }
-    
+
     public BaseRating(string finalComment, Dictionary<string, int> pointsPerCriteria, Dictionary<string, string> commentsPerCriteria)
     {
         FinalComment = finalComment;
         PointsPerCriteria = pointsPerCriteria;
         CommentsPerCriteria = commentsPerCriteria;
-        CreatedAt = DateTime.Now;
-        UpdatedAt = DateTime.Now;
     }
 }
--- EvalPro.Service/src/impl/model/ratings/SupplementaryExamination.cs
+++ EvalPro.Service/src/impl/model/ratings/SupplementaryExamination.cs
@@ -1,16 +1,14 @@
+using EvalProService.impl.model.entities;
+
 namespace EvalProService.impl.model.ratings;
 
-public class SupplementaryExamination
+public class SupplementaryExamination : BaseEntity
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public DateTime UpdatedAt { get; set; } = DateTime.Now;
-    
     public string ChosenTestArea { get; set; }
     public int Points { get; set; }
     public List<string> Questions { get; set; }
 
-    public SupplementaryExamination(string chosenTestArea, int points,  List<string> questions)
+    public SupplementaryExamination(string chosenTestArea, int points, List<string> questions)
     {
         ChosenTestArea = chosenTestArea;
         Points = points;
